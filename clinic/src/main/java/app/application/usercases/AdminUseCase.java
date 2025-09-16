dce591973aaeb90b2354dc110729f77aa2c0efa0
ingrid <ingridvanesasabicharry@gmail.com>
2025-09-16 17:39:05 -0500

    changess

--- clinic/src/main/java/app/application/usercases/AdminUseCase.java
+++ clinic/src/main/java/app/application/usercases/AdminUseCase.java
@@ -1,5 +1,31 @@
 package app.application.usercases;
 
+import app.domain.model.User;
+import app.domain.model.emuns.Role;
+import app.domain.services.CreateUser;
+
 public class AdminUseCase {
+	
+	private CreateUser createUser;
+
+	public void createDoctors(User user) throws Exception {
+		user.setRole(Role.DOCTORS);
+		createUser.createUser(user);
+	}
+	
+	public void createNurse(User user) throws Exception {
+		user.setRole(Role.NURSE);
+		createUser.createUser(user);
+	}
+	
+	public void createNurse(User user) throws Exception {
+		user.setRole(Role.NURSE);
+		createUser.createUser(user);
+	}
+	
+	public void createNurse(User user) throws Exception {
+		user.setRole(Role.NURSE);
+		createUser.createUser(user);
+	}
 
 }
